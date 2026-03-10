abae2f2a56754b09a46e6e301f1cddc51a7342e9
sls_00 <1804048122@qq.com>
2026-03-10 23:30:57 +0800

    feat: dashboard静态部分

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,81 @@
 "use client"
 import { Button } from "@/components/ui/button"
-import {Card,CardHeader,CardTitle,CardDescription,CardAction,CardContent,CardFooter} from "@/components/ui/card"
+import {Card,CardHeader,CardTitle,CardAction,CardContent,CardFooter} from "@/components/ui/card"
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
 export default function Overview() {
+
+  const   cardData = [
+      {
+        title: "Total Sales",
+        description: "Total sales for the current month",
+        value: "0,000",
+        icon: "mdi:cart",
+        color: "bg-[#F59E0B]",
+      },
+      {
+        title: "Total Orders",
+        description: "Total orders for the current month",
+        value: "10",
+        icon: "mdi:package",
+      }
+    ]
+
   return (
     <>
     <div className="flex flex-col h-full w-full gap-4">
@@ -74,8 +148,81 @@ export default function Overview() {
         </main>
         {/* 列表项 */}
         <main className="grid grid-cols-2 gap-4">
-           
-          
+           <Card>
+                 <CardHeader>
+                  <div>
+                     <p className="text-lg font-bold"> Recently Published Articles</p>
+                     <p className="text-sm text-gray-500"> Latest articles from your blog</p>
+                  </div>
+                  <CardContent className="flex-row">
+                    <Table>
+                      <TableCaption>A list of your recent invoices.</TableCaption>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead className="w-[100px]">Invoice</TableHead>
+                          <TableHead>Status</TableHead>
+                          <TableHead>Method</TableHead>
+                          <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {invoices.map((invoice) => (
+                          <TableRow key={invoice.invoice}>
+                            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                            <TableCell>{invoice.paymentStatus}</TableCell>
+                            <TableCell>{invoice.paymentMethod}</TableCell>
+                            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                      <TableFooter>
+                        <TableRow>
+                          <TableCell colSpan={3}>Total</TableCell>
+                          <TableCell className="text-right">$2,500.00</TableCell>
+                        </TableRow>
+                      </TableFooter>
+                    </Table>
+                        
+                  </CardContent>
+                 </CardHeader>
+           </Card>
+             <Card> 
+                               <CardHeader>
+                  <div>
+                     <p className="text-lg font-bold">Comment Activity</p>
+                     <p className="text-sm text-gray-500">Recent comments on your articles</p>
+                  </div>
+                  <CardContent className="flex-row">
+                    <Table>
+                      <TableCaption>A list of your recent invoices.</TableCaption>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead className="w-[100px]">Invoice</TableHead>
+                          <TableHead>Status</TableHead>
+                          <TableHead>Method</TableHead>
+                          <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {invoices.map((invoice) => (
+                          <TableRow key={invoice.invoice}>
+                            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                            <TableCell>{invoice.paymentStatus}</TableCell>
+                            <TableCell>{invoice.paymentMethod}</TableCell>
+                            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                      <TableFooter>
+                        <TableRow>
+                          <TableCell colSpan={3}>Total</TableCell>
+                          <TableCell className="text-right">$2,500.00</TableCell>
+                        </TableRow>
+                      </TableFooter>
+                    </Table>
+                  </CardContent>
+                 </CardHeader>
+             </Card>
         </main>
       </main>
       </div>
